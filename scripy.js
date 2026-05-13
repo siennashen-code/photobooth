54643dd2784380b3eddf4f417e36483d21b18b6a
siennashen-code <sienna_shen@horacemann.org>
2026-05-13 10:09:42 -0400

    added flash

--- scripy.js
+++ scripy.js
@@ -13,7 +13,9 @@ function takePhoto() {
     const context = photo.getContext('2d');
     photo.width = 640;
     photo.height = 480;
+
     context.drawImage(video, 0, 0, 640, 480);
+
     photos.push(photo);
 
     const dataURL = photo.toDataURL('image/png');
@@ -22,6 +24,26 @@ function takePhoto() {
     clicker.download = 'amazingphoto.png';
     clicker.click();
     console.log("hi");
+
+}
+
+async function photoWithFlash() {
+    const [result1, result2] = await Promise.all([flash(), takePhoto()]);
+}
+
+function flash() {
+    const flash = document.createElement('div');
+    flash.textContent = 'hi';
+    flash.id = 'flash';
+    document.body.appendChild(flash);
+    const durationInSeconds = 2;
+    const startTime = Date.now(); // Get start time in milliseconds
+    while (Date.now() - startTime < durationInSeconds * 1000) {
+        // const opacity = -1 * durationInSeconds ^ 2 + durationInSeconds / 2 + 1;
+        flash.style.backgroundColor = `rgba(1, 1, 1, ${opacity})`;
+    }
+    
+    flash.remove();
 
 }
 
@@ -34,18 +56,13 @@ async function countDown(seconds) {
     counter.id = 'counter';
     document.body.appendChild(counter);
 
-    while (seconds >= 0) {
-        if (seconds == 0) {
-            counter.textContent = 'smile!';
-        } else {
-            counter.textContent = seconds;
-        }
+    while (seconds > 0) {
+        counter.textContent = seconds;
         await wait(1);  // actually pause for 1 second
         seconds--;
     }
 
     counter.remove();
-    console.log("hi");
 }
 
 
@@ -53,7 +70,7 @@ async function takePhotos() {
     console.log("im here!");
     for (let i = 0; i < 3; i++) {
         await countDown(3);
-        takePhoto();
+        photoWithFlash();
         await wait(1);
     }
 }
